59195d5ec22c5bd18619d2d2b24d52f1c4494e39
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-10-09 15:47:17 -0500

    created the planting function in tractor.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,22 @@
 import { createPlan } from "./plan.js";
-import { createSoybean } from "./seeds/soybean.js";
 import { addPlant, usePlants } from "./feild.js";
+import { plantSeeds } from "./tractor.js";
 
-const yearlyPlan = createPlan();
-const vege = createSoybean();
-const plants = usePlants();
+// const yearlyPlan = createPlan();
+// const vege = createSoybean();
+// const plants = usePlants();
 
-const test = () => {
-  addPlant(vege);
-  console.log(usePlants());
-};
-test();
+// const test = () => {
+//   addPlant(vege);
+//   console.log(usePlants());
+// };
+// test();
+
+const test = [
+  ["Potato", "Soybean", "Soybean", "Corn"],
+  ["Wheat", "Corn", "Wheat", "Asparagus"],
+  ["Asparagus", "Wheat", "Soybean", "Corn"],
+  ["Asparagus", "Soybean", "Potato", "Wheat"],
+];
+
+plantSeeds(test);
--- src/scripts/seeds/corn.js
+++ src/scripts/seeds/corn.js
@@ -1,8 +1,11 @@
 export const createCorn = () => {
-  const corn = {
-    type: "corn",
-    height: 180,
-    output: 6,
-  };
+  const corn = [
+    {
+      type: "corn",
+      height: 180,
+      output: 6,
+    },
+    { type: "corn", height: 180, output: 6 },
+  ];
   return corn;
 };
